2450d6d8c75ac4d0a778186cd3994b15563f2d4c
LorrainelI <liyuanyulor@gmail.com>
2019-04-21 07:45:58 -0400

    .

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@
 function setup() {
   createCanvas(windowWidth,windowHeight);
   img = loadImage("title.png");
-  video = createVideo("countdown.mp4");
+  video = createVideo("countdown.mp4",loaded);
   font = loadFont("InputMono-Light.ttf");
   video.play();
   video.hide();
